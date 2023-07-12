afbae750d1ca3cab8fbb55c641e2189c52350aeb
quannguyenminh0901 <21010617@st.phenikaa-uni.edu.vn>
2023-07-12 13:41:05 +0700

    week 3.4

--- static/script.js
+++ static/script.js
@@ -1,23 +1,5 @@
-function myFunction() {
-    var menuDropdown = document.getElementById("main-menu-dropdown");
-    menuDropdown.classList.toggle("show");
-    
-    var hamburgerMask = document.querySelector('.hamburger-menu-mask');
-    hamburgerMask.classList.toggle("show");
-}
 
-window.addEventListener("click", function(event) {
-    var hamburgerButton = document.querySelector('.hamburger-button');
-    if (!event.target.matches('.hamburger-button') && !hamburgerButton.contains(event.target)) {
-        var menuDropdown = document.getElementById("main-menu-dropdown");
-        menuDropdown.classList.remove("show");
-        
-        var hamburgerMask = document.querySelector('.hamburger-menu-mask');
-        hamburgerMask.classList.remove("show");
-    }
-});
-
-var modal = document.getElementById('login');
+var modal = document.getElementById('login','register');
 
 window.onclick = function(event) {
     if (event.target == modal) {
